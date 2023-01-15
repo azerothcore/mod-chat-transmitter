d277fc24cb9ab3c163f81c3884ced6c53bbd7dbf
Axel Cocat <ax.cocat@gmail.com>
2023-01-15 13:53:42 +0100

    fix: build warning

--- src/DatabaseManager.cpp
+++ src/DatabaseManager.cpp
@@ -199,7 +199,7 @@ namespace ModChatTransmitter
     bool DatabaseManager::IsValidUtf8(const Binary& data)
     {
         int cnt = 0;
-        for (int i = 0; i < data.size(); i++)
+        for (size_t i = 0; i < data.size(); i++)
         {
             int x = data[i];
             if (!cnt)
